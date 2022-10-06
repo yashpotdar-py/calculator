2a6f8a24cbf9a02cb4ba7d6e0e2d67a1f14173ee
yashpotdar-py <113132066+yashpotdar-py@users.noreply.github.com>
2022-10-06 20:51:17 +0530

    Updating shapes.py
    Optimising the code.

--- shapes.py
+++ shapes.py
@@ -1,150 +1,122 @@
 import math
 
 
-class Dimensions():
+class dimensions:
 
-    
-    def _2d():
+    @staticmethod
+    def second():
+        print("**Shapes Available:\n->Square\n->Rectangle\n->Triangle\n->Circle\n**")
+        shape_choose = input("Choose a Shape: ")
+        shape_choose = shape_choose.lower()
 
-        
-        print("Shapes Available: (For Perimeter Type 'Quadrilateral' For Any 4 Sided Shapes)\
-            \nSquare \nRectangle \nTriangle \nCircle")
-        
-        shape_choose = input("Choose A Shape: ")
-        
-        function_choose = input("Type 'peri' for Perimeter\
-            Type 'area' for Area\n=>")
-        
-        
-        def Perimeter():
-            
-            if shape_choose == "Quadrilateral":
-                side1 = float(input("Side 1: "))
-                side2 = float(input("Side 2: "))
-                side3 = float(input("Side 3: "))
-                side4 = float(input("Side 4: "))
-                perimeter = side1 + side2 + side3 + side4
-                print(f"The Perimeter is: {perimeter} units")
-                        
-            elif shape_choose == "Triangle":
-                side1 = float(input("Side 1: "))
-                side2 = float(input("Side 2: "))
-                side3 = float(input("Side 3: "))
-                perimeter = side1 + side2 + side3                
-                print(f"The Perimeter is: {perimeter} units" )
-            
-            elif shape_choose == "Cirlce":
-                radius = int(input("Radius: "))
+        if shape_choose == 'square':
+            try:
+                side = float(input("Enter the edge length: "))
+                perimeter = side * 4
+                area = side ** 2
+                print(f"Perimeter of the Square is \033[1m\033[92m{perimeter}\033[0m units\
+                    \nArea of the Square is \033[1m\033[92m{area}\033[0m units^2")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        elif shape_choose == 'rectangle':
+            try:
+                length = float(input("Enter the length: "))
+                breadth = float(input("Enter the breadth: "))
+                perimeter = 2 * (length * breadth)
+                area = length * breadth
+                print(f"Perimeter of the Rectangle is \033[1m\033[92m{perimeter}\033[0m units\
+                    \nArea of the Rectangle is \033[1m\033[92m{area}\033[0m units^2")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        elif shape_choose == 'triangle':
+            try:
+                a = float(input("Enter the length of first side: "))
+                b = float(input("Enter the length of second side: "))
+                c = float(input("Enter the length of third side: "))
+                perimeter = a + b + c
+                s = perimeter / 2
+                heron = s * (s - a) * (s - b) * (s - c)
+                area = math.sqrt(heron)
+                print(f"Perimeter of the Triangle is \033[1m\033[92m{perimeter}\033[0m units\
+                    \nArea of the Triangle is \033[1m\033[92m{area}\033[0m units^2")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        elif shape_choose == 'circle':
+            try:
+                radius = float(input("Enter the Radius: "))
                 perimeter = 2 * math.pi * radius
-                print(f"The Perimeter is: {perimeter} units" )
-        
-        
-        def Area():
-            
-            if shape_choose == "Square": 
-                side = float(input("Side: "))
-                area = side*side
-                print(f"Area of Square Is: {area} units^2")
-
-            elif shape_choose == "Rectangle":
-                length = float(input("Length: "))
-                width = float(input("Width: "))
-                area = length * width
-                print(f"Area Of Rectangle Is: {area} units^2")
-            
-            elif shape_choose == "Triangle":
-                base = float(input("Base: "))
-                height = float(input("Height: "))
-                area = 0.5 * base * height
-                print(f"Area Of Triangle Is: {area} units^2")
-
-            elif shape_choose == "Cirlce":
-                radius = float(input("Radius: "))
                 area = math.pi * radius * radius
-                print(f"Are Of Circle Is: {area} units^2")
+                print(f"Perimeter of the Rectangle is \033[1m{perimeter}\033[0m units\
+                    \nArea of the Rectangle is \033[1m{area}\033[0m units^2")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
 
-    
-        if function_choose == "peri":
-            Perimeter()
-
-        elif function_choose == "area":
-            Area()
-    
-
-    def _3d():
-        
-        
-        print("Shapes Available:\
-            \nCube \nCuboid \nSphere")
-        
-        shape_choose1 = input("Choose A Shape: ")
-        
-        function_choose1 = input("Type 'area' for Total Surface Area\
-            Type 'volume' for Volume\n=>")
-
-        
-        def Surface_Area():
-
-
-            if shape_choose1 == "Cube":
-                edge = float(input("Side: "))
-                surface_area = 6 * edge * edge
-                print(f"Total Surface Area Is: {surface_area} units^2")
-
-            elif shape_choose1 == "Cuboid":
-                l = float(input("Length: "))
-                b = float(input("Width: "))
-                h = float(input("Height: "))
-                surface_area = 2 * ((l * b) + (b * h) + (l * h))
-                print(f"Total Surface Area Is: {surface_area} units^2")
-            
-            elif shape_choose1 == "Sphere":
-                radius = float(input("Radius: "))
-                surface_area = 4 * math.pi * radius * radius
-                print(f"Total Surface Area Is: {surface_area} units^2")
-
-        
-        def Volume():
-
-            if shape_choose1 == "Cube":
-                edge = float(input("Side: "))
-                volume = edge * edge * edge
-                print(f"Total Surface Area Is: {volume} units^3")
-
-            elif shape_choose1 == "Cuboid":
-                l = float(input("Length: "))
-                b = float(input("Width: "))
-                h = float(input("Height: "))
-                volume = l * b * h
-                print(f"Total Surface Area Is: {volume} units^3")
-            
-            elif shape_choose1 == "Sphere":
-                radius = float(input("Radius: "))
-                volume = (4/3) * math.pi * radius * radius * radius
-                print(f"Total Surface Area Is: {volume} units^3")
-
-        
-        if function_choose1 == "area":
-            Surface_Area()
-
-        elif function_choose1 == "volume":
-            Volume()
-        
         else:
-            print("ERORR")
+            print("Error")
+
+    @staticmethod
+    def third():
+        print("**Shapes Available:\n->Cube\n->Cuboid\n->Sphere\n**")
+        shape_choose = input("Choose a Shape: ")
+        shape_choose = shape_choose.lower()
+
+        if shape_choose == 'cube':
+            try:
+                side = float(input("Enter the edge length: "))
+                tsa = side * 6
+                volume = side ** 3
+                print(f"Total Surface Area of the Cube is \033[1m\033[92m{tsa}\033[0m units^2\
+                    \nVolume of the Cube is \033[1m\033[92m{volume}\033[0m units^3")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        elif shape_choose == 'cuboid':
+            try:
+                length = float(input("Enter the length: "))
+                breadth = float(input("Enter the breadth: "))
+                height = float(input("Enter the height: "))
+                lsa = 2 * ((length * height) + (breadth * height))
+                tsa = 2 * ((length * breadth) + (breadth * height) + (length * height))
+                volume = length * breadth * height
+                print(f"Lateral Surface Area of the Cuboid is \033[1m\033[92m{lsa}\033[0m units^2\
+                    \nTotal Surface Area of the Rectangle is \033[1m\033[92m{tsa}\033[0m units^2\
+                    \nArea of the Rectangle is \033[1m\033[92m{volume}\033[0m units^3")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        elif shape_choose == 'sphere':
+            try:
+                radius = float(input("Enter the Radius: "))
+                tsa = 2 * math.pi * radius
+                volume = math.pi * radius * radius
+                print(f"Surface Area of the Sphere is \033[1m\033[92m{tsa}\033[0m units\
+                    \nVolume of the Sphere is \033[1m\033[92m{volume}\033[0m units^2")
+            except ValueError:
+                print("Enter A Valid Number")
+                quit()
+
+        else:
+            print("Error")
 
 
-class Shapes:
-    
-    dimension = input("**Choose A Shape(2D/3D):")
+class shape:
+    dimension_choose = input("**Choose A Dimension(2D/3D): ")
+    dimension_choose = dimension_choose.lower()
 
-    if dimension == "2D":
-        Dimensions._2d()
-    
-    elif dimension == "3D":
-        Dimensions._3d()
+    if dimension_choose == '2d':
+        dimensions.second()
+
+    elif dimension_choose == '3d':
+        dimensions.third()
 
     else:
-        print("ERORR")
-
-Shapes()
+        print("Error")
